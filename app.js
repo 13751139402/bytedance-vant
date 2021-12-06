4be6a26d64d153785621a3c33e11617e9f8f439b
bytedance <daixunwei@bytedance.com>
2021-12-06 18:41:53 +0800

    初始化项目

--- app.js
+++ app.js
@@ -1,14 +1,13 @@
 App({
   globalData: {},
 
-  onLaunch() {
-    if (!tt.cloud) {
-      console.error('请使用 2.2.3 或以上的基础库以使用云能力');
-    } else {
-      tt.cloud.init({
-        env: 'production-7dtfw'
-      });
-    }
+  onLaunch() {// if (!tt.cloud) {
+    //   console.error('请使用 2.2.3 或以上的基础库以使用云能力');
+    // } else {
+    //   tt.cloud.init({
+    //     env: 'production-7dtfw'
+    //   });
+    // }
   }
 
 });
